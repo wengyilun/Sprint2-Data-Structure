11fcca657657db6c4183249d462c26a0ee3c9312
Yilun Weng <wengellen@gmail.com>
2020-05-20 20:10:20 -0700

    Finish Link List implementation of Queue

--- queue/queue.py
+++ queue/queue.py
@@ -13,8 +13,11 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-import array as arr
 
+'''
+** Array Implementation
+
+import array as arr
 class Queue:
     def __init__(self):
         self.size = 0
@@ -45,10 +48,38 @@ class Queue:
         output += "]"
         return output
 
-q = Queue()
-q.enqueue(100)
-q.enqueue(101)
-q.enqueue(105)
-print(q)
-q.dequeue()
-print(q)
+'''
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = DoublyLinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
+
+    def dequeue(self):
+        if self.size == 0:
+            return
+        self.size -= 1
+        return self.storage.remove_from_head()
+
+    def __str__(self):
+        return str(self.storage)
+
+
+# q = Queue()
+# q.enqueue(100)
+# q.enqueue(101)
+# q.enqueue(105)
+# print(q)
+# q.dequeue()
+# print(q)
