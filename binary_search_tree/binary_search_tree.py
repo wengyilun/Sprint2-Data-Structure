ad43c6433821b147847ef53eb369de22f5a90545
Yilun Weng <wengellen@gmail.com>
2020-05-26 21:55:08 -0700

    Finish binary search tree PART #1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,66 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        new_node = BSTNode(value)
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = new_node
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = new_node
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+
+        if target < self.value:
+            if self.left is None:
+                return False
+
+            return self.left.contains(target)
+
+        else:
+            if self.right is None:
+                return False
+
+            return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current = self
+        max_value = current.value
+
+        if current.right is None:
+            return self.value
+
+        while current:
+            if max_value < current.value:
+                max_value = current.value
+            current = current.right
+
+        return max_value
+
+
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        queue = [self]
+        while len(queue) > 0:
+            popped = queue.pop()
+
+            if popped is None:
+                continue
+            fn(popped.value)
+            queue.append(popped.left)
+            queue.append(popped.right)
+
 
     # Part 2 -----------------------
 
@@ -59,3 +105,5 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
